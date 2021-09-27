eae93b672f83a319d1253a8e21f1e638a4f20bb4
Vishal Blog <vishalghag47@gmail.com>
2021-09-27 21:46:24 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const jokes = document.querySelector('#joke');
+const jokeBtn = document.querySelector('#jokeBtn');
+
+const generateJokes =async () =>
+{
+    try
+    {
+    const setHeader = {
+        headers: {
+            Accept: "application/json"
+        }
+    }
+
+
+const res = await fetch('https://icanhazdadjoke.com/', setHeader);
+const data = await res.json();
+jokes.innerHTML = data.joke;
+    }catch(err)
+    {
+        console.log(`the error is ${err}`);
+    }
+
+}
+
+jokeBtn.addEventListener('click', generateJokes);
+generateJokes();
